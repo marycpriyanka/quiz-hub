80abd1576c0b022ea2134508a48b6607a7828434
aditore <anthonyditore@gmail.com>
2022-03-24 16:47:11 -0700

    starting to work on login auth helpers

--- controllers/api/quiz/questionRoutes.js
+++ controllers/api/quiz/questionRoutes.js
@@ -1,2 +1,4 @@
 const router = require('express').Router();
 const { Question } = require('../../../models');
+
+module.exports = router;
--- controllers/api/quiz/scoreRoutes.js
+++ controllers/api/quiz/scoreRoutes.js
@@ -1,2 +1,4 @@
 const router = require('express').Router();
 const { Score } = require('../../../models');
+
+module.exports = router;
