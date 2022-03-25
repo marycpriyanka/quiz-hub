e4799814c6fd1a0ed722b5cb7236e9cc78250338
aditore <anthonyditore@gmail.com>
2022-03-24 20:11:57 -0700

    adding user route path to login

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -17,8 +17,52 @@ router.post('/', async (req, res) => {
     }
 });
 
-//login to crerate session
+//login to create session
 router.post('/login', async (req, res) => {
-    
-})
+    try {
+        const userData = await User.findOne({
+            where: {
+                email: req.body.email
+            }
+        });
+
+        if (!userData) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect login credentials, please try again' });
+
+            return;
+        }
+
+        const correctPassword = await userData.checkPassword(req.body.password);
+
+        if (!correctPassword) {
+            res
+            .status(400)
+            .json({ message: 'Incorrect login credentials, please try again' });
+
+            return;
+        }
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.json({ user: userData, message: 'Thank you for logging in!' });
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+router.post('/logout', (req, res) => {
+    if (req.session.logged_in) {
+        req.session.destroy(() => {
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
+
 module.exports = router;
