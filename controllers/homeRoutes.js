4c30b56c441aa9b84af910fe4d3754181ad35f73
Priyanka Mary Chrstine <marycpriyanka@gmail.com>
2022-03-24 21:14:30 -0700

    Add route for getting homepage

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,5 +1,27 @@
 const router = require('express').Router();
-const { User } = require('../models');
+const { User, Quiz } = require('../models');
 const withAuth = require('../utils/auth');
+
+router.get("/", withAuth, async (req, res) => {
+    try {
+        // Gets max of 9 quizzes along with the comments
+        const quizData = await Quiz.findAll({
+            limit: 9
+        });
+
+        // Serialize data so that tempate can read it
+        const quizzes = quizData.map(quiz => quiz.get({ plain: true }));
+
+        // Pass serialized data and session flag into template
+        res.render("homepage", {
+            quizzes,
+            logged_in: req.session.logged_in
+        });
+    } catch (err) {
+        console.log(`Error in getting the homepage: ${err}`);
+        res.status(500).json(err);
+    }
+});
+
 
 module.exports = router;
