22702bf0fed136185d6d852bbbb127ed0f4c381f
aditore <anthonyditore@gmail.com>
2022-03-25 15:17:33 -0700

    starting quiz routes

--- controllers/api/quiz/quizRoutes.js
+++ controllers/api/quiz/quizRoutes.js
@@ -1,4 +1,30 @@
 const router = require('express').Router();
-const { Quiz } = require('../../../models');
+const { Quiz, User, Category } = require('../../../models');
+
+//get all quizzes
+router.get('/', async (req, res) => {
+    try {
+        const quizData = await Quiz.findAll();
+        res.status(200).json(quizData);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
+
+// get quiz by single id
+router.get('/:id', async (req, res) => {
+    try {
+        const chosenQuiz = await Quiz.findByPk(req.params.id);
+
+        if (!chosenQuiz) {
+            res.status(404).json({ message: 'No quiz found' });
+            return;
+        }
+
+        res.status(200).json(chosenQuiz);
+    } catch (err) {
+        res.status(500).json(err);
+    }
+})
 
 module.exports = router;
