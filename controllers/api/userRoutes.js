e2a42f6bc3e8dcd09a3704d74e9481ed2d2e7629
aditore <anthonyditore@gmail.com>
2022-03-24 19:24:44 -0700

    starting user route

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -1,4 +1,24 @@
 const router = require('express').Router();
 const { User } = require('../../models');
 
+//add user into database and start a session
+router.post('/', async (req, res) => {
+    try {
+        const userData = await User.create(req.body);
+
+        req.session.save(() => {
+            req.session.user_id = userData.id;
+            req.session.logged_in = true;
+
+            res.status(200).json(userData);
+        });
+    } catch (err) {
+        res.status(400).json(err);
+    }
+});
+
+//login to crerate session
+router.post('/login', async (req, res) => {
+    
+})
 module.exports = router;
