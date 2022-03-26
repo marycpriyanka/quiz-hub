5daf12b44121890904ebad365bc7156f11553761
Becca Yu <ugsung8970@gmail.com>
2022-03-26 13:10:15 -0700

    commiting to checkt get routes

--- controllers/api/quiz/questionRoutes.js
+++ controllers/api/quiz/questionRoutes.js
@@ -1,9 +1,43 @@
 const router = require('express').Router();
 const { Question } = require('../../../models');
 
-router.get('/:quiz_id', (req,res) => {
-    Question.findOne({})
-    .then()
-})
+router.get('/', (req,res) => {
+    Question.findAll({
+        attributes: [
+            'id',
+            'question_text',
+            'choice1',
+            'choice2',
+            'choice3',
+            'choice4'
+        ],
+    })
+    .then(dbQuestionData => res.json(dbQuestionData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+router.get('/:quiz_id', (req, res) => {
+    Question.findOne({
+        where: {
+            quiz_id: req.params.quiz_id
+        }
+    })
+    .then(dbQuestionData => res.json(dbQuestionData))
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    })
+});
+
+/* router.post('/', (req,res) => {
+    Question.create({
+        question_text: req.body.question_text,
+       
+    })
+}) */
+
 
 module.exports = router;
