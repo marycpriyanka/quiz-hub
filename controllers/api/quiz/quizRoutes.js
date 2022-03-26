d03ed007061dfb124b9a8d71de3dc992f3ec7de6
aditore <anthonyditore@gmail.com>
2022-03-26 13:26:05 -0700

    confirming endpoint of last commit

--- controllers/api/quiz/quizRoutes.js
+++ controllers/api/quiz/quizRoutes.js
@@ -1,5 +1,6 @@
 const router = require('express').Router();
 const { Quiz } = require('../../../models');
+const withAuth = require('../../../utils/auth');
 
 //post quiz
 router.post('/', async (req, res) => {
