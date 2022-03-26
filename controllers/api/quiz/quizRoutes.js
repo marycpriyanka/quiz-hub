2cbdb6c33588f9a15e6370e7f5c6b125269d75ec
aditore <anthonyditore@gmail.com>
2022-03-26 11:57:27 -0700

    no need for get routes

--- controllers/api/quiz/quizRoutes.js
+++ controllers/api/quiz/quizRoutes.js
@@ -1,32 +1,6 @@
 const router = require('express').Router();
 const { Quiz, User, Category } = require('../../../models');
 
-//get all quizzes
-router.get('/', async (req, res) => {
-    try {
-        const quizData = await Quiz.findAll();
-        res.status(200).json(quizData);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
-// get quiz by single id
-router.get('/:id', async (req, res) => {
-    try {
-        const chosenQuiz = await Quiz.findByPk(req.params.id);
-
-        if (!chosenQuiz) {
-            res.status(404).json({ message: 'No quiz found' });
-            return;
-        }
-
-        res.status(200).json(chosenQuiz);
-    } catch (err) {
-        res.status(500).json(err);
-    }
-});
-
 //post quiz
 router.post('/', async (req, res) => {
     try {
