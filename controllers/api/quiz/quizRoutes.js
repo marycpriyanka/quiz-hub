3bfd44260c028a4716dd18680bb9bd1ebc516373
aditore <anthonyditore@gmail.com>
2022-03-26 12:05:40 -0700

    slight change to variable

--- controllers/api/quiz/quizRoutes.js
+++ controllers/api/quiz/quizRoutes.js
@@ -30,7 +30,7 @@ router.delete('/:id', async (req, res) => {
         return;
       }
   
-      res.status(200).json(projectData);
+      res.status(200).json(quizData);
     } catch (err) {
       res.status(500).json(err);
     }
