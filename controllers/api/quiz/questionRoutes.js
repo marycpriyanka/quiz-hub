b1be8239b335feec3b298829caea79ef7fef21a0
Becca Yu <ugsung8970@gmail.com>
2022-03-26 11:23:37 -0700

    commiting to check

--- controllers/api/quiz/questionRoutes.js
+++ controllers/api/quiz/questionRoutes.js
@@ -1,4 +1,9 @@
 const router = require('express').Router();
 const { Question } = require('../../../models');
 
+router.get('/:quiz_id', (req,res) => {
+    Question.findOne({})
+    .then()
+})
+
 module.exports = router;
