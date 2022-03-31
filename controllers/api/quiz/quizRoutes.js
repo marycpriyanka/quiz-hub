5f8c0fb1d14f61e6c1bc60e656fab11715e46986
aditore <anthonyditore@gmail.com>
2022-03-30 18:19:13 -0700

    staging for rebase

--- controllers/api/quiz/quizRoutes.js
+++ controllers/api/quiz/quizRoutes.js
@@ -18,7 +18,7 @@ router.post('/', async (req, res) => {
     try {
       const newQuiz = await Quiz.create({
         ...req.body,
-        //user_id: req.session.user_id,
+        user_id: req.session.user_id,
       });
   
       res.status(200).json(newQuiz);
